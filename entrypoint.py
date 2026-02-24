27d02a92c325d7e3fe8fdaf9e7df6c26ce134870
annapmarin <annapratsmarin@gmail.com>
2026-02-24 20:40:37 +0100

    refactor: add Base to entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -1,13 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from database import Base, engine, get_db
 import uvicorn
 
 app = FastAPI()
 
+Base.metadata.create_all(bind=engine)
+
 @app.get("/")
 def read_root():
-    return {"msg": "Hello World"}
+    return {"msg": "API Funcionant!"}
 
 if __name__ == "__main__":
-    uvicorn.run("entrypoint:app", 
-                host="localhost",
-                reload=True)
+    uvicorn.run("entrypoint:app", host="localhost", reload=True)
